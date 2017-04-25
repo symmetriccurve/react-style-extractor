8c156d92cfda9dd6040cf8098f4e89d157637339
Vikram Belde <belvikram@gmail.com>
2017-04-25 14:23:36 -0700

    flex test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -34,22 +34,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
-        </p>
+      <div className="App" style={{flex:1}}>
+      <div style={{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'tan'}}>
+        
+      </div>
+      <div style={{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'tan'}}>
+        
+      </div>
       </div>
     );
   }
